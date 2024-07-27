1655fb12424c2b4858817519fbb57fc9b302aa8d
Siddhesh Kamire <siddhesh.kamire@dypic.in>
2024-07-27 18:52:34 +0530

    Main Class Creation

---
+++ src/employee/main_class.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class main_class extends JFrame{
+
+    main_class(){
+        setSize(500,500);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new main_class();
+    }
+
+}
