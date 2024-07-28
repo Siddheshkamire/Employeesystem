367eb8d5687044749a659cf15bfe447850392311
Siddhesh Kamire <siddhesh.kamire@dypic.in>
2024-07-28 16:07:20 +0530

    Connection edit

--- src/employee/conn.java
+++ src/employee/conn.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class conn {
@@ -13,8 +14,7 @@ public class conn {
             connection=DriverManager.getConnection( "jdbc:mysql://localhost:3306/employeemanagement","root","siddhesh@28");
             statement=connection.createStatement();
             
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (ClassNotFoundException | SQLException e) {
         }
     }
 }
